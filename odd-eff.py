b397de41c63faee6f516b37b70aee5ee2a1561e5
ishanvarshney7 <69324381+ishanvarshney7@users.noreply.github.com>
2020-09-03 22:49:23 +0530

    Update odd-eff.py

--- odd-eff.py
+++ odd-eff.py
@@ -42,17 +42,11 @@ def bowling():
         print(f'Computer total runs till now {computer_runs}\n')    
     return computer_runs
 
-
-def help():
-    print("HELP")
-    
-
-
 def main_page():
-    #help()
+
     print("\nSelect from the given options")
-    print("1 for batting")
-    print("2 for bowling")
+    print("1 for Batting")
+    print("2 for Bowling")
 
     print("\nYour choice ",end='')
     n=int(input())
@@ -63,6 +57,9 @@ def main_page():
     if n==2:
         computer_runs=bowling()
         player_runs=batting()
+    if n!=1 or n!=2:
+        print("Invalid input!!!")
+        main_page() 
     
     if player_runs>computer_runs:
         print(f'\n\nYou Win by {player_runs-computer_runs} runs :)')
@@ -71,5 +68,57 @@ def main_page():
     else:
         print("Game draw :|")
 
+def main_page1():
 
-main_page()
+    print("\nSelect from the given options")
+    print("1 for Batting")
+    print("2 for Bowling")
+    print("3 for Help")
+
+    print("\nYour choice ",end='')
+    n=int(input())
+
+    if n==3:
+        help()
+    else:
+        
+        if n==1:
+           player_runs=batting()
+           computer_runs=bowling()
+    
+        elif n==2:
+          computer_runs=bowling()
+          player_runs=batting()
+        
+        else:
+            print("Invalid input")
+            main_page1()
+
+    
+    
+        if player_runs>computer_runs:
+            print(f'\n\nYou Win by {player_runs-computer_runs} runs :)')
+        elif player_runs<computer_runs:
+            print(f'\n\nYou Lose by {computer_runs-player_runs} runs :(')
+        else:
+            print("Game draw :|")
+
+
+def help():
+    print("                                                 HELP")
+    print("\n\n\n                                               Instructions \n\n")
+    print("You have to opt a number from 1 to 6 (inluding both of them), similarly the computer will chose a number randomly.")
+    print("You have to do batting and bowling accordingly to your choice and your scores will be recored on each turn then the \nplayer with most runs at the end will win the game")
+
+
+    print("\n\n     For Batting")
+    print("\nIf the number chosen by both are same then you are out else the number selected by you is added to your total score.")
+
+    print("\n\n     For Bowling")
+    print("\nIf the number chosen by both are same then the computer is out else the number selected by the computer is added to \ncomputer's total score.")
+
+    print("\n\n\nPress any key to exit")
+    hold=input()
+    main_page()
+
+main_page1()
